e0154274d0d9e6630141488585be944f57f7f0cf
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2025-05-04 12:28:10 +0800

    1. 增加侧边栏容器 2.增加按钮用于显示隐藏侧边栏容器 3. 增加代码搜索容器并简单实现了功能 4. 增加了显示当前行列的容器 5. 所有的错误提示都精确到列 6. 增加了缩放选项按钮，允许在50% - 150%之间间隔25%改变 7. 修复了当:结尾时后面如果有个空格将会误报的bug 8. 修复了当打出关键字以后加上了:没有参数不报错的bug 9.

--- addons/dialogic_core/ui/dialogic_core_code_editor.gd
+++ addons/dialogic_core/ui/dialogic_core_code_editor.gd
@@ -28,6 +28,18 @@ enum CodeKeyType{
 
 # TODO 对话核心代码编辑器 ===============>变 量<===============
 #region 变量
+# VAR 侧边栏容器
+var slider: VBoxContainer
+# VAR 字体大小菜单按钮
+var font_size_menu_button: MenuButton
+# VAR 代码搜索编辑器
+var code_search_edit: LineEdit
+# VAR 代码搜索到的结果的行数
+var search_code_row : Array[Vector2i]
+# VAR 鼠标行列显示容器
+var cursor_pos_label: Label
+# VAR 显示或隐藏侧边栏按钮
+var show_or_hide_slider_button: Button
 # VAR 代码编辑器
 var code_edit: CodeEdit
 # VAR 高亮解析器
@@ -49,7 +61,13 @@ var not_err : bool = true
 # TODO 对话核心代码编辑器 ===============>虚方法<===============
 #region 常用的虚方法
 func _ready() -> void:
+	slider = %Slider
+	font_size_menu_button = %FontSizeMenuButton
+	code_search_edit = %CodeSearchEdit
 	err_label = %ErrLabel
+	cursor_pos_label = %CursorPosLabel
+	show_or_hide_slider_button = %ShowOrHideSliderButton
+
 	highlighter = highlighter_create()
 	code_member_completions.sort()
 	code_standard_completions.sort()
@@ -70,15 +88,52 @@ func _ready() -> void:
 	)
 	code_edit.syntax_highlighter = highlighter
 	code_edit.add_theme_font_override("font", load("res://JetBrainsMono-ExtraBoldItalic.ttf"))
-	code_edit.add_theme_font_size_override("font_size", 20)
+	code_edit.add_theme_font_size_override("font_size", 15)
 
 	# NOTE 信号链接初始化
+	font_size_menu_button.get_popup().id_pressed.connect(_on_font_size_menu_button_popup_id_pressed)
+	code_search_edit.text_submitted.connect(_on_code_search_edit_text_submitted)
+	show_or_hide_slider_button.pressed.connect(_on_show_or_hide_slider_button_pressed)
+
 	code_edit.code_completion_requested.connect(_on_code_edit_code_completion_requested)
 	code_edit.text_changed.connect(_on_code_edit_text_changed)
+	code_edit.caret_changed.connect(_on_code_edit_caret_changed)
 #endregion
 
 # TODO 对话核心代码编辑器 ===============>信号链接方法<===============
 #region 信号链接方法
+# FUNC 显示或隐藏侧边栏按钮激活方法
+func _on_show_or_hide_slider_button_pressed() -> void:
+	if show_or_hide_slider_button.text == "<":
+		slider.hide()
+		show_or_hide_slider_button.text = ">"
+	elif show_or_hide_slider_button.text == ">":
+		slider.show()
+		show_or_hide_slider_button.text = "<"
+
+# FUNC 修改代码字体大小
+func _on_font_size_menu_button_popup_id_pressed(id: int) -> void:
+	match id:
+		0:
+			font_size_menu_button.text = "50 %"
+			code_edit.add_theme_font_size_override("font_size", 7)
+		1:
+			font_size_menu_button.text = "75 %"
+			code_edit.add_theme_font_size_override("font_size", 11)
+		2:
+			font_size_menu_button.text = "100 %"
+			code_edit.add_theme_font_size_override("font_size", 15)
+		3:
+			font_size_menu_button.text = "125 %"
+			code_edit.add_theme_font_size_override("font_size", 19)
+		4:
+			font_size_menu_button.text = "150 %"
+			code_edit.add_theme_font_size_override("font_size", 22)
+
+# FUNC 光标改变位子的方法
+func _on_code_edit_caret_changed() -> void:
+	cursor_pos_label.text = "%s : %s" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1]
+
 # FUNC 代码编辑器中文本改变时触发的方法
 func _on_code_edit_text_changed() -> void:
 	code_rows = code_edit.text.split("\n")
@@ -123,6 +178,14 @@ func _on_code_edit_code_completion_requested() -> void:
 		for i in code_other_completions:
 			code_edit.add_code_completion_option(CodeEdit.KIND_MEMBER, i, i + " ")
 	code_edit.update_code_completion_options(false)
+
+# FUNC 代码搜索提交方法
+# FIXME 还有点不是很好用，后面修复
+func _on_code_search_edit_text_submitted(new_text: String) -> void:
+	var search_pos : Vector2 = code_edit.search(new_text, CodeEdit.SEARCH_WHOLE_WORDS, code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1)
+	code_edit.set_caret_line(search_pos.y)
+	code_edit.set_caret_column(search_pos.x)
+	code_edit.select_word_under_caret()
 #endregion
 
 # TODO 对话核心代码编辑器 ===============>工具方法<===============
@@ -382,6 +445,8 @@ func code_parser() -> void:
 func code_end_or_spell_error_detection(code_row : String) -> CodeError:
 	if code_row.dedent() == "": return CodeError.OK
 
+	code_row = code_row.strip_edges(false, true)
+
 	var code_row_num : int = code_rows.find(code_row) + 1
 	var code_parts : Array = code_row.split(" ")
 	var tab_num : int = 0
@@ -392,25 +457,33 @@ func code_end_or_spell_error_detection(code_row : String) -> CodeError:
 	tab_num = code_parts[0].count("\t")
 	code_parts[0] = has_tab_part.split("\t")[-1]
 
+	print(code_parts)
+
 	for i in has_end_keys:
-		if code_parts.has(i + ":"): return CodeError.OK
+		if code_parts.has(i + ":"):
+			code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
+			return CodeError.PARAMETER_ERROR
 		if code_parts.has(i):
 			if code_parts.has("："):
-				code_printerr("你可能错误的使用了中文的符号，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能错误的使用了中文的符号，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.ENCODING_ERROR
-			if code_parts.has(":"): return CodeError.OK
+			if code_parts.has(":"):
+				if code_parts[code_parts.find(i) + 1] == ":":
+					code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
+					return CodeError.PARAMETER_ERROR
+				return CodeError.OK
 			if code_parts[-1] != "":
 				if code_parts[-1][-1] == ":": return CodeError.OK
 				if code_parts[-1][-1] == "：":
-					code_printerr("你可能错误的使用了中文的符号，请检查代码第%s行" % code_row_num)
+					code_printerr("你可能错误的使用了中文的符号，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 					return CodeError.ENCODING_ERROR
-			code_printerr("可能是缺失结尾符号，请检查代码第%s行" % code_row_num)
+			code_printerr("可能是缺失结尾符号，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 			return CodeError.MISSING_END_SYMBOL
 
 	for i in not_end_keys:
 		if code_parts.has(i): return CodeError.OK
 
-	code_printerr("可能是拼写错误，请检查代码第%s行" % code_row_num)
+	code_printerr("可能是拼写错误，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 	return CodeError.SPELLING_ERROR
 
 # FUNC 代码值错误检查器
@@ -435,15 +508,15 @@ func code_value_error_detection(code_row : String) -> CodeError:
 		if code_parts.has(i):
 			var code_part_index : int = code_parts.find(i) + 1
 			if code_parts.size() == 1:
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			var code_part : String = code_parts[code_part_index]
 			if code_part == "":
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 			if not code_part.is_valid_float():
-				code_printerr("你可能使用了错误的参数，正确参数应为浮点值，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能使用了错误的参数，正确参数应为浮点值，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 	# NOTE 字符串关键字错误检测
@@ -451,17 +524,17 @@ func code_value_error_detection(code_row : String) -> CodeError:
 		if code_parts.has(i):
 			var code_part_index : int = code_parts.find(i) + 1
 			if code_parts.size() == 1:
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			var code_part : String = code_parts[code_part_index]
 			if code_part == "":
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			if i == "voice":
 				if not code_part.is_absolute_path():
-					code_printerr("你可能使用了错误的参数，正确参数应为文件路径，请检查代码第%s行" % code_row_num)
+					code_printerr("你可能使用了错误的参数，正确参数应为文件路径，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 					return CodeError.PARAMETER_ERROR
 
 	# NOTE 字典关键字错误检测
@@ -469,7 +542,7 @@ func code_value_error_detection(code_row : String) -> CodeError:
 		if code_parts.has(i):
 			var code_part_index : int = code_parts.find(i) + 1
 			if code_parts.size() == 1:
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			var code_part : String
@@ -482,10 +555,10 @@ func code_value_error_detection(code_row : String) -> CodeError:
 				if key.ends_with("]"): break
 
 			if code_part == "":
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 			if not is_valid_format(code_part):
-				code_printerr("你可能使用了错误的参数，正确示例[字符串, 参数1, 参数2, 参数...]，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能使用了错误的参数，正确示例[字符串, 参数1, 参数2, 参数...]，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 	# NOTE 条件关键字错误检测
@@ -493,7 +566,7 @@ func code_value_error_detection(code_row : String) -> CodeError:
 		if code_parts.has(i):
 			var code_part_index : int = code_parts.find(i) + 1
 			if code_parts.size() == 1:
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			var code_part : String
@@ -509,32 +582,32 @@ func code_value_error_detection(code_row : String) -> CodeError:
 				code_part = code_part.left(code_part.length() - 2)
 
 			if code_part == "":
-				code_printerr("你可能缺失了参数，请检查代码第%s行" % code_row_num)
+				code_printerr("你可能缺失了参数，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 			if not is_logic_expression(code_part):
-				code_printerr("请输入正确的条件格式，请检查代码第%s行" % code_row_num)
+				code_printerr("请输入正确的条件格式，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.EXPRESSION_ERROR
 
 	for i in ["emote", "voice", "time", "event", "choice", "end", "continue", "condition"]:
 		if code_parts.has(i):
 			if tab_num < 3:
-				code_printerr("一些关键字的位置不正确，正确位置应该为三个以上的制表符后，请检查代码第%s行" % code_row_num)
+				code_printerr("一些关键字的位置不正确，正确位置应该为三个以上的制表符后，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 				return CodeError.PARAMETER_ERROR
 
 	if code_parts.has("_para"):
 		if tab_num != 0:
-			code_printerr("一些关键字的位置不正确，正确位置应该为无制表符在前，请检查代码第%s行" % code_row_num)
+			code_printerr("一些关键字的位置不正确，正确位置应该为无制表符在前，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 			return CodeError.PARAMETER_ERROR
 
 	if code_parts.has("_role"):
 		if tab_num != 1:
-			code_printerr("一些关键字的位置不正确，正确位置应该为一个制表符后，请检查代码第%s行" % code_row_num)
+			code_printerr("一些关键字的位置不正确，正确位置应该为一个制表符后，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 			return CodeError.PARAMETER_ERROR
 
 	if code_parts.has("text"):
 		if tab_num != 2:
-			code_printerr("一些关键字的位置不正确，正确位置应该为两个制表符后，请检查代码第%s行" % code_row_num)
+			code_printerr("一些关键字的位置不正确，正确位置应该为两个制表符后，请检查代码第%s行%s列" % [code_edit.get_caret_line() + 1, code_edit.get_caret_column() + 1])
 			return CodeError.PARAMETER_ERROR
 
 	return CodeError.OK
